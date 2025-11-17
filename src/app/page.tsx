3b2ff4968319c0acb7e02d742d9fa5fcb53756b5
reendaextra.a extra <reendaextraa01@gmail.com>
2025-11-17 02:43:02 +0000

    adicione galhos marrom no meio da pagina de fundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,25 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="bg-background min-h-screen w-full overflow-x-hidden">
+    <div className="bg-background min-h-screen w-full overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-background via-background to-black/50 z-0"></div>
       
       {/* Background glow elements */}
       <div className="absolute -top-40 -left-40 w-96 h-96 bg-primary/10 rounded-full filter blur-[150px] opacity-50 animate-move-slow"></div>
       <div className="absolute -bottom-40 -right-40 w-96 h-96 bg-primary/10 rounded-full filter blur-[150px] opacity-50 animate-move-slow animation-delay-2000"></div>
        <div className="absolute top-1/4 left-1/2 -translate-x-1/2 w-96 h-96 bg-secondary/5 rounded-full filter blur-[100px] opacity-30 animate-move-slow animation-delay-4000"></div>
+
+       {/* Branch SVG */}
+       <div className="absolute top-0 left-1/2 -translate-x-1/2 h-full w-full max-w-4xl z-0 pointer-events-none">
+          <svg width="100%" height="100%" viewBox="0 0 400 1200" preserveAspectRatio="xMidYMin slice" className="opacity-20 stroke-yellow-900/50">
+            <path d="M 200,0 C 210,150 190,300 200,450 C 210,600 190,750 200,900 C 210,1050 190,1150 200,1200" fill="none" strokeWidth="3"/>
+            <path d="M 200,100 Q 250,150 220,200" fill="none" strokeWidth="2"/>
+            <path d="M 200,300 Q 150,350 180,400" fill="none" strokeWidth="2"/>
+            <path d="M 200,500 Q 260,550 230,620" fill="none" strokeWidth="2"/>
+            <path d="M 200,750 Q 140,800 170,880" fill="none" strokeWidth="2"/>
+            <path d="M 200,1000 Q 240,1050 210,1120" fill="none" strokeWidth="2"/>
+          </svg>
+       </div>
 
        {/* Leaves */}
         <Leaf className="absolute top-[10%] left-[30%] w-16 h-16 opacity-30 animate-float" style={{ animationDuration: '12s', animationDelay: '0s' }} />
